ac50df47587c25e0f72ec5506c1e41548ab97a5d
EwenLo <1lorimerewe2@gmail.com>
2022-09-27 21:07:00 -0400

    changed version

--- src/OctoNDI/__init__.py
+++ src/OctoNDI/__init__.py
@@ -5,7 +5,7 @@ from .OctoNDI import OctoNDI
 __plugin_name__ = "OctoNDI"
 __plugin_version__ = "1.0.0"
 __plugin_description__ = "Access your webcam in HD"
-__plugin_pythoncompat__ = ">=3.8.10"
+__plugin_pythoncompat__ = ">=3.7,4"
 
 
 __plugin_implementation__ = OctoNDI()
